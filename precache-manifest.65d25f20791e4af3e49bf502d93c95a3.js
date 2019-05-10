1cc2004ab8d309d3082219e8440db6b26d240cdd
Corentin Smith <corentin@truckfly.com>
2019-05-10 22:42:10 +0200

    Updates

---
+++ precache-manifest.65d25f20791e4af3e49bf502d93c95a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb8e32c171005ba90b43f560d230e59",
+    "url": "/louiseplantin.fr/index.html"
+  },
+  {
+    "revision": "62b77313d83d404cf841",
+    "url": "/louiseplantin.fr/static/css/main.1da2166d.chunk.css"
+  },
+  {
+    "revision": "c087a9df0ee72af54958",
+    "url": "/louiseplantin.fr/static/js/2.0664e3c5.chunk.js"
+  },
+  {
+    "revision": "62b77313d83d404cf841",
+    "url": "/louiseplantin.fr/static/js/main.355d0ccc.chunk.js"
+  },
+  {
+    "revision": "7ef3ca3c19986e7a1ec7",
+    "url": "/louiseplantin.fr/static/js/runtime~main.f7cc3245.js"
+  }
+]);
