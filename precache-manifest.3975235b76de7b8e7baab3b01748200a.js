d1ae221bfae1f5bbbdc8763fa0e42e78a6f651cb
Corentin Smith <corentin@truckfly.com>
2019-05-18 20:07:50 +0200

    Updates

---
+++ precache-manifest.3975235b76de7b8e7baab3b01748200a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d468d960bac085d428d35ccbf75667ce",
+    "url": "/louiseplantin.fr/index.html"
+  },
+  {
+    "revision": "1fe4a440014662b49bf2",
+    "url": "/louiseplantin.fr/static/css/main.183b931c.chunk.css"
+  },
+  {
+    "revision": "da7ed3f29bbeccee86bf",
+    "url": "/louiseplantin.fr/static/js/2.85de2d88.chunk.js"
+  },
+  {
+    "revision": "1fe4a440014662b49bf2",
+    "url": "/louiseplantin.fr/static/js/main.9c9789c6.chunk.js"
+  },
+  {
+    "revision": "7ef3ca3c19986e7a1ec7",
+    "url": "/louiseplantin.fr/static/js/runtime~main.f7cc3245.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/louiseplantin.fr/precache-manifest.ea76283293489fac07ad1acf5079d545.js"
+  "/louiseplantin.fr/precache-manifest.3975235b76de7b8e7baab3b01748200a.js"
 );
 
 self.addEventListener('message', (event) => {
